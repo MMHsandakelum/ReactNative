e1948c187a2b27d7e715deed58b731c1eff7f436
MMHsandakelum <mmharshasandakelum@gmail.com>
2022-09-07 12:33:05 +0530

    Add task fuction

--- firstapp/App.js
+++ firstapp/App.js
@@ -1,18 +1,56 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Task from "./components/Task";
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
   return (
     <View style={styles.container}>
       {/* Todays task */}
       <View style={styles.taskwrapper}>
         <Text style={styles.title}>My First ReactNative application</Text>
         <View style={styles.items}>
+          {taskItems.map((item, index) => {
+            return <Task key={index} text={item} />;
+          })}
           <Task text={"Task 1"} />
           <Task text={"Task 2"} />
         </View>
       </View>
+
+      {/* write a task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writetaskwrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a Task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addwrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -20,16 +58,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#CBF0FF",
   },
   taskwrapper: {
     paddingTop: 110,
     paddingHorizontal: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: "bold",
-    color: "red",
+    color: "black",
   },
-  items: {},
+  items: {
+    marginTop: 30,
+  },
+  writetaskwrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "white",
+    width: 250,
+    borderRadius: 60,
+    borderColor: "#c0c0c0",
+    borderWidth: 1,
+  },
+  addwrapper: {
+    height: 60,
+    width: 60,
+    backgroundColor: "white",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#c0c0c0",
+    borderWidth: 1,
+  },
+  addText: {},
 });
--- firstapp/components/Task.js
+++ firstapp/components/Task.js
@@ -1,14 +1,52 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const Task = (props) => {
   return (
-    <View>
-      <Text>{props.text}</Text>
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circle}></View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#fff",
+    padding: 15,
+    marginTop: 10,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#73E0FF",
+    opacity: 0.6,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  circle: {
+    width: 12,
+    height: 12,
+    borderColor: "#73E0FF",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
 
 export default Task;
